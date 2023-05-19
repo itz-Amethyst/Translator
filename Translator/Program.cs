83f61b76c007d0c1e562c9e5f886abd5a89b7f14
itz-amethyst <pransermi@gmail.com>
2023-05-19 20:41:28 +0430

    feat: Implementing idea

--- Translator/Program.cs
+++ Translator/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Text;
 using System.Web;
+using Spectre.Console;
 
 namespace Translator;
 
@@ -10,6 +11,68 @@ class Program
 
     static void Main(string[] args)
     {
+
+        var text = "";
+
+         void BootUp()
+        {
+            AnsiConsole.Write(
+                    new FigletText("Void Translator")
+                        .LeftJustified()
+                        .Color(Color.Teal)
+                );
+
+            var root = new Tree("Root")
+                .Guide(TreeGuide.BoldLine);
+            root.AddNode("[purple] Translator.sln[/]");
+
+            var Bin = root.AddNode("[yellow]Bin[/]");
+
+            var debug = Bin.AddNode("[green]Debug[/]");
+
+            debug.AddNode("[cyan]Translator.exe [/]");
+
+            var src = root.AddNode("[red] Src [/]");
+
+            src.AddNode("[red] Program.cs[/]");
+
+            src.AddNode("[red] VoiceVoxUtility.cs[/]");
+
+            src.AddNode("[red] BootUp.cs[/]");
+
+            src.AddNode("[red] Translate.cs[/]");
+
+
+
+            // Render the tree
+            AnsiConsole.Write(root);
+
+            AnsiConsole.Progress()
+                .StartAsync(async ctx=>
+                {
+                    // Define tasks
+                    var task1 = ctx.AddTask("[green]Reticulating splines[/]");
+                    var task2 = ctx.AddTask("[green]Folding space[/]");
+
+                    while (!ctx.IsFinished)
+                    {
+                        await Task.Delay(22);
+                        task1.Increment(1.5);
+                        task2.Increment(0.5);
+                    }
+
+
+                });
+
+
+        }
+
+        void GetText()
+        {
+            Console.WriteLine("Enter translate text :");
+            text = Console.ReadLine();
+        }
+
         String Translate(String word)
         {
             var toLanguage = "ja";//Japanese
@@ -32,15 +95,21 @@ class Program
             {
                 return "Error";
             }
+
         }
 
         //var selectLanguage = Prompt.Select("Select Input Language", new[] { "en", "da" });
         //? To make System Read Japanese
         Console.OutputEncoding = Encoding.Unicode;
 
-        Console.WriteLine("Enter translate text :");
-        string text = Console.ReadLine();
-        Translate(text);
+        BootUp();
+
+
+        //if (GetText())
+        //{
+        //    Translate(text);
+        //}
+
 
     }
 
